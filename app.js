6d0ec84bc4a39682a97ef0145558ebce489f02a2
Antonio4645 <2038623@alu.murciaeduca.es>
2026-03-10 16:50:15 +0100

    Añadida PWA completa con logo e icono

--- app.js
+++ app.js
@@ -135,3 +135,9 @@ function eliminar(){
 
 // Generar calendario al cargar
 generarCalendario();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("Service Worker registrado"))
+    .catch((err) => console.log("Error al registrar Service Worker:", err));
+}
